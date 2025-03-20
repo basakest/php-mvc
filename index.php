b76f6fe76900331bb389e40bb274caad305eb2ff
basakest <ab1652759879@gmail.com>
2025-03-20 10:15:54 +0800

    remove duplicated code

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
 <?php
 
-$controller = $_GET['controller'] ?? 'home';
+$controller = ucfirst($_GET['controller'] ?? 'home');
 $action = $_GET['action'] ?? 'index';
 
-if ($controller === 'products') {
-    require 'src/controllers/products.php';
-    $objController = new Products();
-} else {
-    require 'src/controllers/home.php';
-    $objController = new Home();
-}
-
-if ($action === 'index') {
-    $objController->index();
-} elseif ($action === 'show') {
-    $objController->show();
-} else {
-    $objController->index();
-}
+require "src/controllers/{$controller}.php";
+$objController = new $controller();
+$objController->$action();
