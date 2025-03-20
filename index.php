f2e699da08047959e6f698fd0029616b26ac85c0
basakest <ab1652759879@gmail.com>
2025-03-20 08:03:11 +0800

    add html to render page

--- index.php
+++ index.php
@@ -8,4 +8,22 @@ $pdo = new PDO($dsn, $user, $password);
 $sql = 'SELECT * FROM `product`;';
 $statement = $pdo->query($sql,  PDO::FETCH_ASSOC);
 $products = $statement->fetchAll();
-$products;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Products</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<?php foreach ($products as $product): ?>
+    <h2>
+        <?= htmlspecialchars($product['name']); ?>
+    </h2>
+    <p>
+        <?= htmlspecialchars($product['description']); ?>
+    </p>
+<?php endforeach; ?>
+</body>
+</html>
