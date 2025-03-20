e5ba8b4cca36dd9304a98c5cdcd168e22181f819
basakest <ab1652759879@gmail.com>
2025-03-20 08:49:21 +0800

    add view.php

--- index.php
+++ index.php
@@ -3,21 +3,5 @@
 require './model.php';
 
 $products = (new Model())->getData();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Products</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-<?php foreach ($products as $product): ?>
-    <h2>
-        <?= htmlspecialchars($product['name']); ?>
-    </h2>
-    <p>
-        <?= htmlspecialchars($product['description']); ?>
-    </p>
-<?php endforeach; ?>
-</body>
-</html>
+
+require './view.php';
