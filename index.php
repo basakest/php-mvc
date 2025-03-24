9bb873003e9e60531f6788be3c32bf747e3a6c0a
basakest <ab1652759879@gmail.com>
2025-03-24 09:34:59 +0800

    make url pretty

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-$controller = ucfirst($_GET['controller'] ?? 'home');
-$action = $_GET['action'] ?? 'index';
+// uri without query string
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$items = array_values(array_filter(explode('/', $path)));
+list($controller, $action) = $items + ['home', 'index'];
 
 require "src/controllers/{$controller}.php";
 $objController = new $controller();
