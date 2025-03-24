65d91a8e9d01c51adbae9a59a6f286cb64964b82
basakest <ab1652759879@gmail.com>
2025-03-24 10:14:16 +0800

    add basic route table

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
+require './src/router.php';
+
+$router = new Router();
+$router->add('/', ['controller' => 'home', 'action' => 'index']);
+$router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
+$router->add('/products', ['controller' => 'products', 'action' => 'index']);
+
 // uri without query string
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$items = array_values(array_filter(explode('/', $path)));
-list($controller, $action) = $items + ['home', 'index'];
+$items = $router->match($path);
+if ( ! $items) {
+    exit('no matched route');
+}
+extract($items);
+// $items = array_values(array_filter(explode('/', $path)));
+// list($controller, $action) = $items + ['home', 'index'];
 
 require "src/controllers/{$controller}.php";
 $objController = new $controller();
